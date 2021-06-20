05d83c036c9e1fbf873c67c937ee0c509ee7b680
youn9jo <youn9jo@gmail.com>
2021-06-20 11:56:25 +0900

    21.06.20. 11:54 카운트마다 조건식 달아줘서 해당 범위 안에서만 사이즈 변경됨, 임의의  최대치 이후에는 애니메이션 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
   const [detecting, setDetecting] = useState(false);
   const [support, setSupport] = useState("");
   const twoRef = useRef();
+  const faceRef = useRef();
+  const wrapperRef = useRef();
 
   let countNum;
 
@@ -36,8 +38,6 @@ function App() {
   if(support){
     let promise = navigator.mediaDevices.getUserMedia(constraints);
     promise.then(callback).catch(err => console.log(err));
-  } else {
-    alert('지금 쓰고 계시는 거 마이크 안먹힙니다')
   }
 
   function callback(stream) {
@@ -46,6 +46,7 @@ function App() {
       let analyser = ctx.createAnalyser();
       
       mic.connect(analyser);
+
       countNum = 0;
 
       if(detecting){
@@ -63,13 +64,34 @@ function App() {
 
           var average = values / length;
 
-          if(average > 100){
-              twoRef.current.style.width = `${Math.round(average - 80)}%`;
-              countNum = countNum + 1;
-              console.log(Math.round(average), countNum);
-          } else {
-            twoRef.current.style.width = `10%`;
-            countNum = 0;
+          if(average > 10){
+              countNum = countNum + 1; 
+              if (countNum < 100 && countNum >= 0) {
+                wrapperRef.current.style.width = (average <= 15) ? `${Math.round(average)}%` : "10%";
+              } else if(countNum < 300 && countNum >= 100) {
+                wrapperRef.current.style.width = (average <= 30) ? `${Math.round(average)}%` : "15%"; 
+                wrapperRef.current.style.width = "15%";
+                faceRef.current.style.top = "15%";
+                faceRef.current.style.width = "35%";
+                console.log('15');
+              } else if ( countNum < 500 && countNum >= 300) {
+                wrapperRef.current.style.width = (average <= 50) ? `${Math.round(average)}%` : "30%"; 
+                wrapperRef.current.style.width = "30%";
+                faceRef.current.style.top = "5%";
+                faceRef.current.style.width = "25%";
+                console.log('30');
+              } else if ( countNum < 700 && countNum >= 500 ) {
+                // animation 추가 
+                wrapperRef.current.style.animation = "y-axis 3s ease-in-out";
+                twoRef.current.style.animation = "x-axis 3s linear";
+              }
+          } else { // 초기화
+              wrapperRef.current.style.width = "10%";
+              faceRef.current.style.top = "25%";
+              faceRef.current.style.width = "47%";
+              countNum = 0;
+              wrapperRef.current.style.animation = "none";
+              twoRef.current.style.animation = "none";
           }
           requestAnimationFrame(play);
         }
@@ -92,11 +114,11 @@ function App() {
         <div className = "one">
           <img src = { one } alt = "one" />
         </div>
-        <div className = "two" ref = { twoRef }>
-          <img src = { two } alt = "two" />
-        </div>
-        <div className = "three">
-          <img src = { three } alt = "three" />
+        <div className = "wrapper" ref = { wrapperRef }>
+          <div className = "two" ref = { twoRef }>
+            <img className = "head" src = { two } alt = "two" />
+            <img className = "face" ref = { faceRef } src = { three } alt = "three" />
+          </div>
         </div>
         {/* <div className = "four">
           <img src = { four } alt = "four" />
